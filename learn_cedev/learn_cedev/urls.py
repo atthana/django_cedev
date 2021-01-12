33de28d766917207d2b11f196030054615aad9d7
atthana-p <atthana.p@codium.co>
2021-01-12 18:09:26 +0700

    แก้อันมะกี้ รันไม่ขึ้น

--- learn_cedev/learn_cedev/urls.py
+++ learn_cedev/learn_cedev/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(('myapp.urls', 'myapp'), namespace='myapp '))
+    path('', include(('myapp.urls', 'myapp'), namespace='myapp')),
 ]
