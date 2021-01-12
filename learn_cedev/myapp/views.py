3e30e9ee28d0a6f6a2aaba5f1f0a5ce8aa55554c
atthana-p <atthana.p@codium.co>
2021-01-12 17:41:31 +0700

    3. ใช้ re_path() ขึ้นต้นด้วย r ลงท้ายด้วย /$ เพื่อ strict url มากขึ้นว่าต้องต้องตามนี้

--- learn_cedev/myapp/views.py
+++ learn_cedev/myapp/views.py
@@ -5,3 +5,8 @@ from django.http import HttpResponse
 def hello(request, id):
     greeting = 'Your id: {0}'.format(id)
     return HttpResponse("<h1>Hello Q-Electronics</h1>" + greeting)
+
+
+def article(request):
+    return HttpResponse('Article')
+
