e0770cd1157e3310e42d95dcd5df4b8c3f9f6b4b
atthana-p <atthana.p@codium.co>
2021-01-12 18:27:46 +0700

    8. เซตที่อยู่ของ template ใน settings.py เพื่อใ้ห้ Django รู้ว่าจะเก็บ template ไว้ที่ folder ไหน พร้อมกับสร้างFolder template มารองรับเมื่อ view เรียก function render() ก้อจะมาที่นี่ แต่ถ้าเราใช้ FE Angular ตรงนี้ก้อปล่อยว่างได้นะ

--- learn_cedev/myapp/urls.py
+++ learn_cedev/myapp/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
+    path('', views.index, name='index'),
     path('hello/<int:id>', views.hello, name='hello2'),
     re_path(r'article/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)/$', views.article, name='article')
 
--- learn_cedev/myapp/views.py
+++ learn_cedev/myapp/views.py
@@ -2,6 +2,11 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 
+def index(request):
+    return render(request, 'index.html')
+    # มันคือให้ render ไปที่ index.html ซึ่งอยู่ที่ templates/index.html ตามที่เราตั้งไว้ที่ settings line 61 นะ
+
+
 def hello(request, id):
     greeting = 'Your id: {0}'.format(id)
     return HttpResponse("<h1>Hello Q-Electronics</h1>" + greeting)
