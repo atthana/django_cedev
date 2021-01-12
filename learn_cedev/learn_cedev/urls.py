323ef2a921e5e22ee76328fb6b1f8db594f49fc7
atthana-p <atthana.p@codium.co>
2021-01-12 17:57:08 +0700

    5. ตั้งชื่อ path ได้เพื่อให้สะดวกในการทำ link ไปเรียกมัน

--- learn_cedev/learn_cedev/urls.py
+++ learn_cedev/learn_cedev/urls.py
@@ -18,9 +18,9 @@ from django.urls import path, re_path
 from myapp import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('hello/<int:id>', views.hello),
-    re_path(r'article/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)/$', views.article)
+    path('admin/', admin.site.urls, name='hello1'),
+    path('hello/<int:id>', views.hello, name='hello2'),
+    re_path(r'article/(?P<year>[0-9]{4})/(?P<slug>[\w-]+)/$', views.article, name='article')
     # year=0-9 และ 4 หลักเท่านั้น, slug=ตัว word ที่มี "-" และ + คือความยาวกี่ตัวก็ได้ "
     # test ด้วย this url "http://localhost:8000/article/2099/Q-Electronics/"
 ]
