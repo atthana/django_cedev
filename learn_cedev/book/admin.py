3c3d6768212f228362b38a3aab0373f5f5232e7a
atthana-p <atthana.p@codium.co>
2021-01-13 23:15:09 +0700

    16. จัดการ admin page ต่อโดยเราสามารถสร้าง model ในหน้านี้เพื่อ  view, filter, search  ได้, จัดกลุ่ม field ต่างๆ

--- learn_cedev/book/admin.py
+++ learn_cedev/book/admin.py
@@ -1,6 +1,19 @@
 from django.contrib import admin
 from .models import Category, Author, Book
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['code', 'name', 'category', 'price', 'published']
+    list_filter = ['published']
+    search_fields = ['code', 'name']
+    prepopulated_fields = {'slug': ['name']}
+    # สร้าง slug จาก field 'name' เวลาเราเพ่ิมข้อมูลใน Name ข้อมูลจะเพิ่มใน Slug: ให้เลยอัตโนมัติโดยเอา space มาแทนด้วย "-" นะ
+    fieldsets = (
+        (None, {'fields':['code', 'slug', 'name', 'description', 'price', 'published']}),
+        ('Category', {'fields': ['category', 'author'], 'classes': ['collapse']}),
+    )
+    # fieldsets เป็นการจัดกลุ่มให้เป็น set นะในหน้า admin เพื่อให้ไม่รกตาเกินไป
+
+
 admin.site.register(Category)
 admin.site.register(Author)
-admin.site.register(Book)
+admin.site.register(Book, BookAdmin)
