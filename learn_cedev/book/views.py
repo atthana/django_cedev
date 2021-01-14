4d846482e32c880a18ab44982d96980731f933f6
atthana-p <atthana.p@codium.co>
2021-01-15 00:25:20 +0700

    24. สร้าง view โดยกำหนด queryset ว่าจะเอาอะไรมาโชว์บ้าง และสร้าง urls มาชี้ + ให้แสดงผลผ่าน html (ตอนที่ 5.1-12:58) นะ

--- learn_cedev/book/views.py
+++ learn_cedev/book/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Category, Book
 
-# Create your views here.
+
+def index(request):
+    categories = Category.objects.all()
+    books = Book.objects.all().order_by('id')
+    return render(request, 'book/index.html', {
+        'categories': categories,
+        'books': books,
+    })
