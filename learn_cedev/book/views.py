db6c105fe6758e9671186c96addcb3c6ce42490d
atthana-p <atthana.p@codium.co>
2021-01-25 01:17:09 +0700

    31. [Ep5.2: 8:03] ทำ Pagination ด้วย Paginator() โดยต้องทำทั้งใน views.py และ html, work well na.

--- learn_cedev/book/views.py
+++ learn_cedev/book/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import Category, Book
 
 
@@ -7,10 +8,20 @@ def index(request):
     books = Book.objects.all().order_by('id')
 
     categ_id = request.GET.get('categoryid')
-    print(categ_id)
-    print(type(categ_id))
     if categ_id:
         books = books.filter(category_id=categ_id)
+
+    paginator = Paginator(books, 2) # ระบุ object และจำนวนต่อเพจ
+    print("paginator = ", paginator)
+    page = request.GET.get('page')
+    print("page = ", page)
+    # breakpoint()
+    try:
+        books = paginator.page(page)
+    except PageNotAnInteger:
+        books = paginator.page(1)  #  ถ้าหน้าเพจมีปัญหาก้อให้มันอยู่ที่เพจ 1 เสมอ
+    except EmptyPage:  # หน้าสุดท้ายหรือหน้าน้อยเกินไปก็ให้ดึงข้อมูลทั้งหมดมาใส่ในตัวแปร books 
+        books = paginator.page(paginator.num_pages)
 
     return render(request, 'book/index.html', {
         'categories': categories,
