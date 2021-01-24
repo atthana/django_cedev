0009faca4a56b7885221095c983235e63945d4a0
atthana-p <atthana.p@codium.co>
2021-01-24 22:25:06 +0700

    28.  ผูกวิธีการ filter จาก django เข้ากับ url เพื่อให้ส่ง id เข้ามาใน query string  หลังจากนั้นก้อ render และส่งค่าใน object กลับไปด้วยนะ, ต้องเขียนทั้งใน html และ views.py

--- learn_cedev/book/views.py
+++ learn_cedev/book/views.py
@@ -5,7 +5,15 @@ from .models import Category, Book
 def index(request):
     categories = Category.objects.all()
     books = Book.objects.all().order_by('id')
+
+    categ_id = request.GET.get('categoryid')
+    print(categ_id)
+    print(type(categ_id))
+    if categ_id:
+        books = books.filter(category_id=categ_id)
+
     return render(request, 'book/index.html', {
         'categories': categories,
         'books': books,
+        'categ_id': categ_id,
     })
