5e68f215e68b7f62878c9a51ffb089dcce2b0025
atthana-p <atthana.p@codium.co>
2021-02-01 00:24:10 +0700

    44. มาทำส่วนที่รับ post method เข้ามาบ้างใน django (views.py พร้อมคำอธิบายนะ), ส่วนใน html เพิ่ม 'novalidate' เข้าไปด้วยนะ เพื่อให้ validate เฉพาะ BE เท่านั้น ไม่งั้นมันจะไม่ยอมไป validate ที่ BE.

--- learn_cedev/book/views.py
+++ learn_cedev/book/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
+# import เข้ามาเพิ่มเพื่อจัดการกับ method 'POST' นะ
+from slugify import slugify
+from django.contrib import messages
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
 from .forms import BookForm
 from .models import Category, Book
 
@@ -13,7 +19,7 @@ def index(request):
     if categ_id:
         books = books.filter(category_id=categ_id)
 
-    paginator = Paginator(books, 2) # ระบุ object และจำนวนต่อเพจ
+    paginator = Paginator(books, 10) # ระบุ object และจำนวนต่อเพจ
     print("paginator = ", paginator)
     page = request.GET.get('page')
     print("page = ", page)
@@ -41,6 +47,21 @@ def detail(request, slug):
 
 def book_add(request):
     form = BookForm()
-    return render(request, 'book/add.html', {
+
+    # ส่วนที่เพิ่มขึ้นมา เพื่อมาเช็ค form ที่ post เข้ามาจาก FE นะ (code ในการ add, save ข้อมูลลง db)
+    # พอเรากดปุ่ม save book มันจะวิ่งกลับมาที่ function เดิมของมันคืออันนี้ เราจึงต้องมาเช็ค if ที่นี่
+    if request.method == 'POST':
+        form = BookForm(request.POST, request.FILES)  # ถ้า post เข้ามาก้อมาอ่านข้อมูลจาก request ทั้งข้อความและไฟล์อัพโหลดด้วย พออ่านเสร็จก้อ generate กลับมาให้อยู่ในรูปของ object form เหมือนเดิมนะ
+        if form.is_valid():  # มา validate ต่อว่าฟอห์มถูกต้องรึป่าว default จะ validate จากค่า null+blank, โดย default จะ validate จากค่า blank เป็นหลัก
+            book = form.save(commit=False)  # ถ้า validate ผ่านนะจึงมาทำต่อที่นี่ สร้าง object book จาก form โดย save แบบหลอกๆให้กลายเป็น object book นะ (พูดง่ายๆคือ เปลี่ยน form ให้เป็น object book นะ)
+            book.slug = slugify(book.name)  # gen slug ขึั้นมาใส่ใน book.slug (ใช้ lib ช่วยนะ)
+            book.published = True  # หนังสือใหม่ที่เราจะเซฟ ให้เป็น auto publish นะ
+            book.save()  # พอพร้อมหมดแล้วก้อให้ save ข้อมูล
+            form.save_m2m()  # ถ้า book เรามีการใช้ author ซึ่งเป็นแบบ many to many นะ จะต้องใช้ .save_m2m() ด้วยนะ เป็นคำสั่งในการ save field ในฝั่ง many
+            messages.success(request, 'Save success')  # ถ้า save สำเร็จก้อให้มันส่ง message กลับไปที่หน้าจอให้ user รู้
+            return HttpResponseRedirect(reverse('book:index', kwargs={})) # พอ save ผ่าน ก็ redirect กลับไปที่หน้า index และไม่ได้ส่ง param อะไรกลับไปนะ เลยเป็น dict ว่างๆ
+        messages.error(request, 'Save failed !')  # กรณี save fail ก็ให้ส่ง message กลับไปโชว์นะ
+
+    return render(request, 'book/add.html', {  # โดยถ้า save fail, message ก้อจะกลับมาโชว์ที่เดิมนะ โดย code ชุดนี้จะไม่ทำนะ กรณี success เพราะได้ return redirect ไปก่อนแล้ว
         'form': form
     })
