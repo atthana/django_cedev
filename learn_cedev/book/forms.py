0e0d9763d8af5db0ace30e66124e5f48e4761d39
atthana-p <atthana.p@codium.co>
2021-02-01 01:03:04 +0700

    46. เป็นการทำ custom validation ตามที่เราต้องการ ไม่ใช่แค่การเช็ค null, blank และโชว์ค่าตามที่เราต้องการ (เราจะเขียน __init__ คือให้มันมาทำที่นี่ ก่อนที่จะทำของ standart django นะ เรียกว่าการเขียน event hook).

--- learn_cedev/book/forms.py
+++ learn_cedev/book/forms.py
@@ -6,3 +6,16 @@ class BookForm(forms.ModelForm):
     class Meta:
         model = Book
         exclude = ['id', 'slug', 'published', 'created', 'updated']  # ไม่เอาอะไรบ้าง จะตรงข้ามกับแบบเก่าที่เอาอะไรบ้าง
+
+    def __init__(self, *args, **kwargs):  # อันนี้เป็น pattern ของการทำ function hook นะ (custom validator)
+        super(BookForm, self).__init__(*args, **kwargs) # ส่ง parameter ตัวเดิมกลับไปนะ
+        self.fields['code'].error_messages = {
+            'required': 'Please enter book code',
+        }
+        self.fields['name'].error_messages = {
+            'required': 'Please enter book name',
+        }
+        self.fields['price'].error_messages = {
+            'required': 'Please enter book price',
+            'invalid': 'Please enter a valid book price'
+        }
