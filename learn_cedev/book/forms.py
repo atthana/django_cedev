9ed8c53f1535f3b5543353d5dcf2992e92745e7e
atthana-p <atthana.p@codium.co>
2021-02-01 01:10:16 +0700

    47. ต่อมาจะ validate ตระกูลที่เป็น dropdownlist หรือ listbox ซึ่งเราต้องมาเขียน code handle เอง (ตรง __init__ มันจะใช้ไม่ได้นะ) เราจะ hook function clean(self) มาใช้

--- learn_cedev/book/forms.py
+++ learn_cedev/book/forms.py
@@ -8,7 +8,7 @@ class BookForm(forms.ModelForm):
         exclude = ['id', 'slug', 'published', 'created', 'updated']  # ไม่เอาอะไรบ้าง จะตรงข้ามกับแบบเก่าที่เอาอะไรบ้าง
 
     def __init__(self, *args, **kwargs):  # อันนี้เป็น pattern ของการทำ function hook นะ (custom validator)
-        super(BookForm, self).__init__(*args, **kwargs) # ส่ง parameter ตัวเดิมกลับไปนะ
+        super(BookForm, self).__init__(*args, **kwargs) # ไปอ่านข้อมูลจาก BookForm มา และส่ง parameter ตัวเดิมกลับไปนะ
         self.fields['code'].error_messages = {
             'required': 'Please enter book code',
         }
@@ -19,3 +19,10 @@ class BookForm(forms.ModelForm):
             'required': 'Please enter book price',
             'invalid': 'Please enter a valid book price'
         }
+
+    def clean(self):  # เป็นการ hook function clean() มาใช้ 
+        cd = super(BookForm, self).clean()
+        if not cd.get('category'):  # ถ้าไม่่มี key 'category' นะก้อค่อยทำ
+            self.add_error('category', 'Please select category name')
+        if not cd.get('author'):
+            self.add_error('author', 'Please choose an author name')
